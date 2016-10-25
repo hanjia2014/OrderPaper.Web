688679ef801f2383c261ae69769346cb07598512
Datacom <Datacom@CALLUMG-LT002>
2016-10-25 13:25:22 +1300

    add enum and change border color

--- src/OrderPaper.Web/wwwroot/app/models/OrderPaper.ts
+++ src/OrderPaper.Web/wwwroot/app/models/OrderPaper.ts
@@ -9,8 +9,16 @@ export class OrderPaper {
     Number: number;
     SittingHours: string;
     Sections: Array<Section>;
-
+    Progress: ProgressStep;
     constructor() {
         this.Sections = new Array<Section>();
     }
+}
+
+export enum ProgressStep {
+    None = 0,
+    Preview = 1,
+    Word = 2,
+    Print = 3,
+    Publish = 4
 }
