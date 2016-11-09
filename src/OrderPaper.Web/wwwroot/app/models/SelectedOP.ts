739f918120284ce57d3b11ebfcfa39b9314e36e7
Datacom <Datacom@CALLUMG-LT002>
2016-11-10 08:24:20 +1300

    add function

--- src/OrderPaper.Web/wwwroot/app/models/SelectedOP.ts
+++ src/OrderPaper.Web/wwwroot/app/models/SelectedOP.ts
@@ -1,4 +1,10 @@
-﻿export class SelectedOP {
+﻿import { OrderPaper }   from '../models/orderpaper';
+export class SelectedOP {
     Id: number;
     Saved: boolean;
+    Value: OrderPaper;
+
+    public compare = (orderPaper: OrderPaper): boolean => {
+        return JSON.stringify(this.Value) == JSON.stringify(orderPaper);
+    }
 }
