636a7816ccec51d6ae2ecec338d6087d36b66a88
Datacom <Datacom@CALLUMG-LT002>
2016-11-11 12:30:46 +1300

    progress step check

--- src/OrderPaper.Web/wwwroot/app/models/OrderPaper.ts
+++ src/OrderPaper.Web/wwwroot/app/models/OrderPaper.ts
@@ -1,6 +1,7 @@
 ﻿import { Status }                                   from './constants';
 import { Item, MotionItem, BillItem, ReportItem }   from './Items';
 import { Section }                                  from './Section';
+import { AppConstants }                             from '../settings/app.constants';
 
 export class OrderPaper {
     Id: number;
@@ -14,5 +15,21 @@ export class OrderPaper {
     constructor() {
         this.Sections = new Array<Section>();
         this.PublishingProgress = new Array<string>();
+    }
+
+    public containPreview = (): boolean => {
+        return this.PublishingProgress.indexOf(AppConstants.PROGRESS_PREVIEW) >= 0;
+    }
+
+    public containWord = (): boolean => {
+        return this.PublishingProgress.indexOf(AppConstants.PROGRESS_WORD) >= 0;
+    }
+
+    public containPublish = (): boolean => {
+        return this.PublishingProgress.indexOf(AppConstants.PROGRESS_PUBLISH) >= 0;
+    }
+
+    public containPrint = (): boolean => {
+        return this.PublishingProgress.indexOf(AppConstants.PROGRESS_PRINT) >= 0;
     }
 }
