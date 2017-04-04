d8ac8465b3a3c44dbe39a0fea84955196bb7b9cf
Datacom <Datacom@CALLUMG-LT002>
2017-04-05 11:20:49 +1200

    add subheading to section

--- src/OrderPaper.Web/Models/Section.cs
+++ src/OrderPaper.Web/Models/Section.cs
@@ -9,10 +9,12 @@ namespace OC.OrderPapersWeb.Models
         public string Name;
         public List<Item> Items;
         public List<List<Item>> Groups;
+        public Subheading Subheading;
         public Section()
         {
             Items = new List<Item>();
             Groups = new List<List<Item>>();
+            Subheading = new Subheading();
         }
     }
 }
